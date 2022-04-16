87497ff3032099a1617e1b1d8171fca9561451e9
Adam Scott <newberry94005@yahoo.com>
2022-04-16 12:37:48 -0400

    Added Copy of DB Rules

---
+++ functions/dbRules.js
@@ -0,0 +1,42 @@
+{
+    "rules": {
+      
+      "bookings":{
+          ".read": false,
+          ".write": false
+      },
+      "customers":{
+          ".read": true,
+          ".write": false
+      },
+      "groupcost":{
+          ".read": true,
+          ".write": false
+      },
+      "holds":{
+          ".read": true,
+          ".write": false
+      },
+      "hoursInfo":{
+          ".read": true,
+          ".write": false
+      },
+      "roomCombos":{
+          ".read": true,
+          ".write": false
+      },
+      "roomInfo":{
+          ".read": true,
+          ".write": false
+      } 
+    }
+  }
+
+/*
+  {
+    "rules": {
+      ".read": true,
+      ".write": false
+    }
+  }
+  */
